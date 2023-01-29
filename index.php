6daca46cb48fcb8eae711b542d7d666e65cd378b
GeremiasSousa <sousageremias9@Gmail.com>
2023-01-29 19:24:53 -0300

    estilizando a tabela das imgs

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,18 +10,51 @@
         history.pushState("Object", document.title, './');
     </script>
 </head>
+
 <body>
     <form action="./upload.php" enctype="multipart/form-data" method="post">
         <label for="file">Selecione arquivo</label>
         <input type="file" name="file" id="file"><br>
         <button type="submit">Enviar arquivo</button>
     </form>
-    <?php 
-        if(isset($_GET['upload'])){
-            if($_GET['upload'] === 'ok'){
-                echo 'Upload feito com sucessso';
-            }
+    <br>
+    <?php
+    if (isset($_GET['upload'])) {
+        if ($_GET['upload'] === 'ok') {
+            echo 'Upload feito com sucessso <br>';
         }
+    }
     ?>
+    <br>
+    <table  style="text-align: center;width:100%;background:#3434;border-radius:10px;padding:1rem;">
+        <thead>
+            <tr">
+                <th>Id</th>
+                <th>Path</th>
+                <th>Data Cadastro</th>
+                <th>Usuario</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            require('conexao.php');
+            $res = $conexao->query("SELECT * FROM arquivos");
+            while ($row = $res->fetch_assoc()) {
+                // echo "<img src='" . $row['path'] . "'>";
+                echo "<tr>";
+                echo "<td>" . $row['id'] . "</td>";
+                echo "<td><a href='" . $row['path'] . "' target='_blank'>  <img src='" . $row['path'] . "'style='width:50px; heith:5opx;'/> </a></td>";
+                echo "<td>" . $row['data_upload'] . "</td>";
+                echo "<td>" . $row['usuario'] . "</td>";
+                echo "</tr>";
+            }
+
+            ?>
+        </tbody>
+    </table>
+
+
+
 </body>
+
 </html>
